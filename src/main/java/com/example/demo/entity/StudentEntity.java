c98f22793a5b976f039fb82e7111ee010102e119
bala-amypo <bala@amypo.in>
2025-12-19 06:26:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -13,42 +13,44 @@ private String name;
 private String email;
 private String password;
 private Date created;
-public StudentEntity(){}
-public StudentEntity(String name,Integer id,String email,String password,Date created){
-    this.name =name;
-    this.id = id;
-    this.email = email;
-    this.password=password;
-    this.created = created;
+
 }
-public String getName(){
-    return name;
-}
-public String getEmail(){
-    return email;
-}
-public String getPassword(){
-    return password;
-}
-public Date getCreated(){
-return created;
-}
-public Integer getId(){
-    return id;
-}
-public void setName(String name){
-    this.name= name;
-}
-public void setEmail(String email){
-    this.email=email;
-}
-public void setPassword(String password){
-    this.password = password;
-}
-public void setCreated(Date created){
-    this.created = created;
-}
-public void setId(Integer id){
-    this.id = id;
-}
-}
+// public StudentEntity(){}
+// public StudentEntity(String name,Integer id,String email,String password,Date created){
+//     this.name =name;
+//     this.id = id;
+//     this.email = email;
+//     this.password=password;
+//     this.created = created;
+// }
+// public String getName(){
+//     return name;
+// }
+// public String getEmail(){
+//     return email;
+// }
+// public String getPassword(){
+//     return password;
+// }
+// public Date getCreated(){
+// return created;
+// }
+// public Integer getId(){
+//     return id;
+// }
+// public void setName(String name){
+//     this.name= name;
+// }
+// public void setEmail(String email){
+//     this.email=email;
+// }
+// public void setPassword(String password){
+//     this.password = password;
+// }
+// public void setCreated(Date created){
+//     this.created = created;
+// }
+// public void setId(Integer id){
+//     this.id = id;
+// }
+// }
