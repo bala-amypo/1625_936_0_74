57def7059b4e1695fb9de46a1806451ce0e75669
bala-amypo <bala@amypo.in>
2025-12-19 07:54:26 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/Timestampservice.java
+++ src/main/java/com/example/demo/service/Impl/Timestampservice.java
@@ -1,8 +1,6 @@
 package com.example.demo.service;
 import java.util.List;
-import com.example.demo.entity.StudentEntity;
-public interface Studentservice{
-StudentEntity postdata(StudentEntity entity);
+import com.example.demo.entity.Timestamp;
+public interface Timestampservice{
+Timestamp posttime(Timestamp entity);
 }
-
-
